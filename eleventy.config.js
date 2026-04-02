d803db8e90468caf24c897ecbb9a85a14aec1aeb
Anon <88592342+anon-123456789@users.noreply.github.com>
2026-04-02 02:55:25 -0400

    preliminary 11ty migration

---
+++ eleventy.config.js
@@ -0,0 +1,8 @@
+export default function (eleventyConfig) {
+    eleventyConfig.setInputDirectory("site")
+	eleventyConfig.addPassthroughCopy("site/css")
+    eleventyConfig.addPassthroughCopy("site/files")
+    eleventyConfig.addPassthroughCopy("site/img")
+    eleventyConfig.addPassthroughCopy("site/js")
+    eleventyConfig.addPassthroughCopy("site/google231018a64ef976db.html")
+}
